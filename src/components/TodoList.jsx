d1bd72d08efe8795bda7224aa73ba00e423eeeff
adrianapquijanoc <adrianapquijano>
2025-09-08 02:43:16 -0500

    Actualización de App.jsx y conexión Firebase

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,17 @@
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-
 export default function TodoList({ todos, completeTodo }) {
   return (
-    <TransitionGroup component="ul" className="todo-list">
+    <ul className="todo-list">
       {todos.map((todo) => (
-        <CSSTransition key={todo.id} timeout={300} classNames="fade">
-          <li className="todo-item">
-            {todo.task}
+        <li
+          key={todo.id}
+          className={`todo-item ${todo.completed ? "completed" : ""}`}
+        >
+          {todo.task}
+          {!todo.completed && (
             <button onClick={() => completeTodo(todo.id)}>Completar</button>
-          </li>
-        </CSSTransition>
+          )}
+        </li>
       ))}
-    </TransitionGroup>
+    </ul>
   );
 }
