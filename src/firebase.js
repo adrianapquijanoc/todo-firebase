0c8b5cd1581fa2dd63627f3f9d04cf24f3d7032d
adrianapquijanoc <adrianapquijano>
2025-09-08 01:31:50 -0500

    Actualización de App.jsx y conexión Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+   apiKey: "AIzaSyAys82cr-iLv8nLAtq_f1PVvBGIkfug81k",
+  authDomain: "todo-list-bf432.firebaseapp.com",
+  projectId: "todo-list-bf432",
+  storageBucket: "todo-list-bf432.firebasestorage.app",
+  messagingSenderId: "350455958141",
+  appId: "1:350455958141:web:f37330a43436cbd6147f14",
+  measurementId: "G-LEN0REWSP4" // opcional
 };
 
 const app = initializeApp(firebaseConfig);
